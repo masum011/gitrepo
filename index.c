59ed3c581ee58539526f8bbeea9e44c540af336a
ryan <masumre1010@gmail.com>
2021-03-05 02:09:24 +0530

    welcome commted

---
+++ index.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+    int main(int argc, char const *argv[])
+    {
+        printf("Welcome\n");
+        for (int i = 0; i < 10; i++)
+        {
+            printf("%d\t",i);
+        }
+       
+        
+        return 0;
+    }
+    
