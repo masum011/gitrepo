5287bc6851a62fc11a4f83006757e5d8f1551d6d
ryan <masumre1010@gmail.com>
2021-03-05 02:23:42 +0530

    dev branch switch

--- index.c
+++ index.c
@@ -7,7 +7,9 @@
         {
             printf("%d\t\n",i);
         }
-       printf("Hello ,Git command\n");
+       printf("Hello ,Git command\n\n");
+       printf("Hello i'm writing dev barnch for testing parpase\n");
+
         
         return 0;
     }
